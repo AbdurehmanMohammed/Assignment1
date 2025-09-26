489cf189461fcab878df1c5d2c6edb8c5a7376b5
AbdurehmanMohammed <amoha808@my.centennialcollege.ca>
2025-09-25 22:39:17 -0400

    Commit1
    Myfirst commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,20 @@
-function About() {
+import React from "react";
+
+function AboutMe() {
   return (
     <div>
-      <h1>About Me</h1>
-      <p>This is the about page of my portfolio site.</p>
-
+      <h1>Abdurehman Mohammed</h1>
+      <img src="/images/me.jpg" alt="Abdurehman Mohammed" width="200" />
+      <p>
+        I am a beginner web developer passionate about creating interactive
+        and user-friendly websites. I enjoy learning new technologies and
+        building projects that solve real-world problems.
+      </p>
       <a href="/Coop-Resume.pdf" target="_blank" rel="noopener noreferrer">
-        Download My Resume
+        View My Resume
       </a>
     </div>
   );
 }
 
-export default About;
+export default AboutMe;
